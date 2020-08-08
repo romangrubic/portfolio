1f8c78db878097ff9a04af229382fa1858d196e3
Roman Grubic <grubic.roman@gmail.com>
2020-08-08 19:19:48 +0100

    Updating CV and adding GSAP to Landing page

--- src/containers/Personal/Landing/Landing.js
+++ src/containers/Personal/Landing/Landing.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import classes from './Landing.module.css';
-// import gsap, { SteppedEase } from 'gsap';
+import gsap, { SteppedEase } from 'gsap';
 
 const Landing = (props) => {
-    // let LandingText = useRef(null);
+    let LandingText = useRef(null);
+    let SecText = useRef(null);
 
-    // useEffect(() => {
-    //     let boxTl = gsap.timeline();
+    useEffect(() => {
+        let boxTl = gsap.timeline();
 
-    //     boxTl.fromTo(
-    //         LandingText,
-    //         5,
-    //         { width: '0' },
-    //         {
-    //             width: '450px',
-    //             ease: SteppedEase.config(37),
-    //         },
-    //         0
-    //     );
-
-    //     boxTl.fromTo(
-    //         LandingText,
-    //         0.5,
-    //         {
-    //             borderColor: 'rgba(255,255,255,0.75)',
-    //         },
-    //         {
-    //             borderColor: 'rgba(255,255,255,0)',
-    //             repeat: -1,
-    //             ease: SteppedEase.config(37),
-    //         },
-    //         0
-    //     );
-    // }, []);
+        boxTl
+            .fromTo(
+                LandingText,
+                5,
+                { width: '0' },
+                {
+                    width: '620px',
+                    ease: SteppedEase.config(37),
+                },
+                0
+            )
+            .fromTo(
+                LandingText,
+                0.5,
+                {
+                    borderColor: 'rgba(255,255,255,0.75)',
+                },
+                {
+                    borderColor: 'rgba(255,255,255,0)',
+                    repeat: -1,
+                    ease: SteppedEase.config(37),
+                },
+                0
+            )
+            .to(SecText, 2, { delay: 4.5, opacity: '1' });
+    }, []);
 
     return (
         <div>
@@ -41,19 +43,23 @@ const Landing = (props) => {
                 <br></br>
                 <h2>Web developer who can solve your problem <br></br>or help with an existing project</h2> */}
                 <h1
-                // ref={(el) => {
-                //     LandingText = el;
-                // }}
+                    ref={(el) => {
+                        LandingText = el;
+                    }}
                 >
-                    Hello, <br></br>It's a pleasure to meet you.
+                    Hello, it's a pleasure to meet you.
                 </h1>
-                <br></br>
-                <h2>
+
+                <h3
+                    ref={(el) => {
+                        SecText = el;
+                    }}
+                >
                     The purpose of this site is to serve as extension to my
-                    resume, provide information to potential employers while
-                    demonstrating my skills. <br></br>Looking for a job at the
-                    moment!
-                </h2>
+                    resume,<br></br> provide information to potential employers
+                    while demonstrating my skills. <br></br>Looking for a job at
+                    the moment!
+                </h3>
             </div>
             <div className={classes.area}>
                 <ul className={classes.circles}>
