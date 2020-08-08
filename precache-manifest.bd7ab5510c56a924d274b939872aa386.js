f46536b6a37666f4834ed2ab0b8db923edcaa8ce
Roman Grubic <grubic.roman@gmail.com>
2020-08-08 17:07:24 +0100

    Updates

---
+++ precache-manifest.bd7ab5510c56a924d274b939872aa386.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7e487c1bcc3036f71a826185cc4efb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc483631ea135df8f39",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "276e4e720818156bb5bb",
+    "url": "/static/css/main.41e71a9f.chunk.css"
+  },
+  {
+    "revision": "5fc483631ea135df8f39",
+    "url": "/static/js/2.f613e444.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f613e444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276e4e720818156bb5bb",
+    "url": "/static/js/main.e4d0b6c1.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "48a8ff4006918d7d99aadc0a017ac914",
+    "url": "/static/media/garaks-clothiers.48a8ff40.png"
+  },
+  {
+    "revision": "173beb66fc4acee94b6b017a366849b2",
+    "url": "/static/media/profilepic.173beb66.png"
+  },
+  {
+    "revision": "a2ce0d0cf255822135d78fbaaec6b7e9",
+    "url": "/static/media/responsive-burger.a2ce0d0c.png"
+  },
+  {
+    "revision": "bd7aaeb1aed62035e82b1f0dfdc89809",
+    "url": "/static/media/responsive-snoop.bd7aaeb1.png"
+  },
+  {
+    "revision": "dee73c71ef3f05ed6d745473335612e9",
+    "url": "/static/media/responsive-star-trek.dee73c71.png"
+  },
+  {
+    "revision": "96756ea6b5dd8246f20f281a5057e8de",
+    "url": "/static/media/responsive-weather.96756ea6.png"
+  },
+  {
+    "revision": "01f751c400800bfbba299fdc5df88b90",
+    "url": "/static/media/responsive.01f751c4.png"
+  }
+]);
